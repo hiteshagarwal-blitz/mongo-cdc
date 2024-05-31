db012a4053d3e6c20f7d2d0d0ed1e46fdf75d2c0
Hitesh Agarwal <hitesh.agarwal@blitzscale.co>
2024-05-31 22:56:25 +0530

    add change stream updates formatting

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,52 @@ import {mongodb} from './config';
 
 const {host, port, db_name, replset} = mongodb;
 
+type RequiredChangeStreanType =
+  | mongoose.mongo.ChangeStreamInsertDocument
+  | mongoose.mongo.ChangeStreamUpdateDocument;
+
+const cdcParserForUpdateOp = (
+  changeEvent: mongoose.mongo.ChangeStreamUpdateDocument
+) => {
+  return {
+    db: changeEvent.ns.db,
+    collection: changeEvent.ns.coll,
+    document_id: changeEvent.documentKey._id,
+    updated_fields: changeEvent.updateDescription.updatedFields,
+    removed_fields: changeEvent.updateDescription.removedFields,
+    truncated_arrays: changeEvent.updateDescription.truncatedArrays,
+    timestamp: changeEvent.clusterTime,
+    resume_token: changeEvent._id,
+  };
+};
+
+const cdcParserForInsertOp = (
+  changeEvent: mongoose.mongo.ChangeStreamInsertDocument
+) => {
+  return {
+    db: changeEvent.ns.db,
+    collection: changeEvent.ns.coll,
+    document_id: changeEvent.documentKey._id,
+    full_document: changeEvent.fullDocument,
+    timestamp: changeEvent.clusterTime,
+    resume_token: changeEvent._id,
+  };
+};
+
+const cdcParser = (changeEvent: RequiredChangeStreanType) => {
+  let formattedChangeEvent;
+  switch (changeEvent.operationType) {
+    case 'insert':
+      formattedChangeEvent = cdcParserForInsertOp(changeEvent);
+      break;
+    case 'update':
+      formattedChangeEvent = cdcParserForUpdateOp(changeEvent);
+      break;
+  }
+
+  console.log('CDC Event Data:\n', formattedChangeEvent);
+};
+
 (async () => {
   try {
     const connection = await mongoose
@@ -21,18 +67,13 @@ const {host, port, db_name, replset} = mongodb;
 
     const pipeline = [
       {
-        $match: {operationType: 'insert'},
-      },
-      {
-        $match: {operationType: 'update'},
+        $match: {operationType: {$in: ['insert', 'update']}},
       },
     ];
 
     const watchCursor = connection.watch(pipeline, {startAfter: resumeToken});
-    watchCursor.on('change', event => {
-      console.log(event, {depth: null});
-      resumeToken = event._id;
-      console.log('resumeToken>>', resumeToken);
+    watchCursor.on('change', (event: RequiredChangeStreanType) => {
+      cdcParser(event);
     });
 
     process.on('SIGINT', () => {
