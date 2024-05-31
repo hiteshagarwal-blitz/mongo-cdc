081a09018fe1f66bd00bcc9c067f8a36e838225e
Hitesh Agarwal <hitesh.agarwal@blitzscale.co>
2024-05-31 18:02:56 +0530

    add: error handling

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,21 @@ import {mongodb} from './config';
 const {host, port, db_name, replset} = mongodb;
 
 (async () => {
-  const connection = await mongoose
-    .createConnection(`mongodb://${host}:${port}/${db_name}`, {
-      readPreference: 'secondaryPreferred',
-      replicaSet: replset,
-    })
-    .asPromise();
+  try {
+    const connection = await mongoose
+      .createConnection(`mongodb://${host}:${port}/${db_name}`, {
+        readPreference: 'secondaryPreferred',
+        replicaSet: replset,
+      })
+      .asPromise();
 
-  let resumeAfter;
+    let resumeAfter;
 
-  const watchCursor = connection.watch([], {resumeAfter});
-  watchCursor.on('change', next => {
-    console.log(next);
-  });
+    const watchCursor = connection.watch([], {resumeAfter});
+    watchCursor.on('change', next => {
+      console.log(next);
+    });
+  } catch (err) {
+    console.log('>>>Error>>>>\n', err);
+  }
 })();
