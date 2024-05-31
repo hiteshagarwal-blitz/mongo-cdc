dfe2e41379c6b4325cb6ce2c70fc897cec0e1315
Hitesh Agarwal <hitesh.agarwal@blitzscale.co>
2024-05-31 18:45:00 +0530

    update docker image path

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t asia-south1-docker.pkg.dev/blitzscale-prod-project/blitzscale-prod/mongo-cdc:latest .
-docker push asia-south1-docker.pkg.dev/blitzscale-prod-project/blitzscale-prod/mongo-cdc:latest
+docker build -t asia-south1-docker.pkg.dev/blitzscale-dev-project/blitzscale-dev/mongo-cdc:latest .
+docker push asia-south1-docker.pkg.dev/blitzscale-dev-project/blitzscale-dev/mongo-cdc:latest
 kubectl rollout restart deployment mongo-cdc -n subs
