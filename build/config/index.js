c31258f2942175a831797532a9ac088f778cf2f2
Hitesh Agarwal <hitesh.agarwal@blitzscale.co>
2024-05-31 18:31:56 +0530

    updtae package

--- build/config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mongodb = void 0;
-const nconf = require("nconf");
-nconf.argv().env().file({ file: 'build/config/config.json' });
-exports.mongodb = nconf.get('mongodb');
-//# sourceMappingURL=index.js.map
